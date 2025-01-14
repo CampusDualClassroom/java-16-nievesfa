b15d20776c26fd4bd5fb05de1e7e2ef0c4fe4faf
nievesfa <nievesfacorro@gmail.com>
2025-01-14 16:56:31 +0100

    test ok

--- src/main/java/com/campusdual/classroom/Exercise16.java
+++ src/main/java/com/campusdual/classroom/Exercise16.java
@@ -6,11 +6,22 @@ public class Exercise16 {
 		Plane plane = new Plane("Boeing");
 		Tractor tractor = new Tractor(3500);
 
-		plane.start();
-		tractor.start();
 
+
+		plane.start();
+		plane.stop();
+		plane.maintenance();
+
+		tractor.start();
+		tractor.stop();
+		tractor.maintenance();
+
+		plane.takeOff();
 		plane.fly();
+		plane.land();
+
 		tractor.forward();
+		tractor.backward();
 
 	}
 
